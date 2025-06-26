65f384a3f70605fe839bf87c8f3b39b42058ee19
meilan <meilanulfia@gmail.com>
2025-06-26 15:57:12 +0700

    add Step.6 If the value in the data field of new Node is less than that of parent

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -72,6 +72,13 @@ public:
             return;
         }
 
+        // Step.6: If the value in the data field of new Node is less than that of parent
+        if (x < parent->info)
+        {
+            
+        }
+
+
         
     }
 };
