828cdf3074323efe75c658e8a43c472cd81f9132
meilan <meilanulfia@gmail.com>
2025-06-26 15:58:55 +0700

    add sub 6 for Make the left chlid of parent point to the new Node dan Exit

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -75,7 +75,11 @@ public:
         // Step.6: If the value in the data field of new Node is less than that of parent
         if (x < parent->info)
         {
-            
+            // 6a : Make the left chlid of parent point to the new Node
+            parent->leftchild = newNode;
+
+            // 6b : Exit
+            return;
         }
 
 
